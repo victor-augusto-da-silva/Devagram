40f77ce4423692885751b78a635511bb9d59e8e4
Victor Augusto da Silva <victor1231967@hotmail.com>
2024-05-11 13:09:23 -0300

    Request tela de login

---
+++ devagram/pages/api/cadastro.ts
@@ -0,0 +1,26 @@
+import type {NextApiRequest,NextApiResponse} from 'next';
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
+import type {CadastroRequisicao} from '../../types/CadastroRequisicao';
+
+
+const endpointCadastro = (req: NextApiRequest, 
+    res:NextApiResponse<RespostaPadraoMsg>)=>{
+        if(req.method == 'POST')
+            {
+                const usuario = req.body as CadastroRequisicao;
+                    if(!usuario.nome || usuario.nome.length < 2){
+                    return res.status(400).json({erro : 'Nome invalido'})
+                    }
+                    if(!usuario.email || usuario.email.length < 5 || !usuario.email.includes('@') || !usuario.email.includes('.'))
+                        {
+                            return res.status(400).json({erro : 'Email inválido'});
+                        }
+                    if(!usuario.senha || usuario.senha.length < 4){
+                        return res.status(400).json({erro : 'Senha invalida'});
+                    }
+                    return res.status(200).json({msg: 'Dados corretos'});
+            }
+        return res.status(405).json({erro : 'Metodo informado não é valido'})
+    }
+
+    export default endpointCadastro;
