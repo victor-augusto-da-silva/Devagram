0fca4e3416a5ed57eac47b4a45bc990a63fec2c0
Victor Augusto da Silva <victor1231967@hotmail.com>
2024-05-15 15:57:18 -0300

    Alteração na autenticação buscando pelo banco

--- devagram/pages/api/cadastro.ts
+++ devagram/pages/api/cadastro.ts
@@ -4,6 +4,7 @@ import { conectarMongoDB} from '../../middlewares/conectarMongoDB';
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
 import type { CadastroRequisicao } from '../../types/CadastroRequisicao';
 import { UsuarioModel } from '../../Models/UsuarioModel';
+import md5 from 'md5';
 
 const endpointCadastro = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
     if (req.method === 'POST') {
@@ -17,8 +18,19 @@ const endpointCadastro = async (req: NextApiRequest, res: NextApiResponse<Respos
         if (!usuario.senha || usuario.senha.length < 4) {
             return res.status(400).json({ erro: 'Senha inválida' });
         }
+        //Validação se ja existe usuario com o mesmo email
+        const usuariosComMesmoEmail = await UsuarioModel.find({email: usuario.email});
+        if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0 ){
+            return res.status(400).json({erro: 'Ja existe uma conta com o e-mail informado'})
+        }
+
         // Salva no banco de dados
-        await UsuarioModel.create(usuario);
+        const ususarioSalvo = {
+            nome: usuario.nome,
+            email: usuario.email,
+            senha: md5(usuario.senha),
+        }
+        await UsuarioModel.create(ususarioSalvo);
         return res.status(200).json({ msg: 'Usuário criado com sucesso' });
     }
     return res.status(405).json({ erro: 'Método informado não é válido' });
--- devagram/pages/api/login.ts
+++ devagram/pages/api/login.ts
@@ -1,21 +1,24 @@
-import type {NextApiRequest,NextApiResponse} from 'next';
-import { conectarMongoDB} from '../../middlewares/conectarMongoDB';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
+import md5 from 'md5';
+import { UsuarioModel } from '../../models/UsuarioModel'; // Certifique-se de que o caminho está correto e é case-sensitive
 
-const endpointLogin = (
+const endpointLogin = async (
     req: NextApiRequest,
-    /*O objeto só aceita respostas padrões*/ 
-    res: NextApiResponse<RespostaPadraoMsg>) =>{
-        if (req.method == 'POST'){
-            const {login,senha} =  req.body 
+    res: NextApiResponse<RespostaPadraoMsg>
+) => {
+    if (req.method === 'POST') { // Use '===' para comparação estrita
+        const { login, senha } = req.body;
 
-            if(login === 'admin@admin.com' &&  senha === 'Admin@123') {
-               return res.status(200).json({msg : 'Usuario autenticado com sucesso'});
-            }
-            return res.status(400).json({erro : 'Usuario ou senha invalidos'});
+        const usuarioEncontrado = await UsuarioModel.find({ email: login, senha: md5(senha) });
+        if (usuarioEncontrado && usuarioEncontrado.length > 0) {
+            const usuarioLogado = usuarioEncontrado[0];
+            return res.status(200).json({ msg: `Usuário ${usuarioLogado.nome} autenticado com sucesso` });
         }
-        return res.status(405).json({erro : 'Metodo informado não é valido'});
+        return res.status(400).json({ erro: 'Usuário ou senha inválidos' });
     }
-//primeiro conecta depois roda o endpoint
-    export default conectarMongoDB(endpointLogin)
+    return res.status(405).json({ erro: 'Método informado não é válido' });
+};
 
+export default conectarMongoDB(endpointLogin);
