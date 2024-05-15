df8a852e58766c8226c36f98f94adc3626bc0eed
Victor Augusto da Silva <victor1231967@hotmail.com>
2024-05-15 20:00:53 -0300

    Integracao JWT

--- devagram/pages/api/login.ts
+++ devagram/pages/api/login.ts
@@ -4,32 +4,33 @@ import { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
 import md5 from 'md5';
 import { UsuarioModel } from '../../Models/UsuarioModel';
 import jwt from 'jsonwebtoken';
+import {loginResposta} from '../../types/LoginResposta'
 
 const endpointLogin = async (
     req: NextApiRequest,
-    res: NextApiResponse<RespostaPadraoMsg>
+    res: NextApiResponse<RespostaPadraoMsg | loginResposta>
 ) => {
-    const {MINHA_CHAVE_JWT} = process.env;
-    if(!MINHA_CHAVE_JWT){
-        res.status(500).json({erro: 'ENV JWT não informada'});  
+    if (req.method !== 'POST') {
+        return res.status(405).json({ erro: 'Método informado não é válido' });
     }
 
-    if (req.method === 'POST') { // Use '===' para comparação estrita
-        const { login, senha } = req.body;
-
-        const usuarioEncontrado = await UsuarioModel.find({ email: login, senha: md5(senha) });
-        if (usuarioEncontrado && usuarioEncontrado.length > 0) {
-            const usuarioLogado = usuarioEncontrado[0];
-
-            const token = jwt.sign({ _id: usuarioEncontrado[0]._id }, MINHA_CHAVE_JWT);
-
-            
-            
-            return res.status(200).json({ msg: `Usuário ${usuarioLogado.nome} autenticado com sucesso` });
-        }
-        return res.status(400).json({ erro: 'Usuário ou senha inválidos' });
+    const MINHA_CHAVE_JWT = process.env.MINHA_CHAVE_JWT;
+    if (!MINHA_CHAVE_JWT) {
+        return res.status(500).json({ erro: 'ENV JWT não informada' });
     }
-    return res.status(405).json({ erro: 'Método informado não é válido' });
+
+    const { login, senha } = req.body;
+    const usuarioEncontrado = await UsuarioModel.find({ email: login, senha: md5(senha) });
+    if (usuarioEncontrado && usuarioEncontrado.length > 0) {
+        const usuarioLogado = usuarioEncontrado[0];
+
+        const token = jwt.sign({ _id: usuarioLogado._id }, MINHA_CHAVE_JWT);
+        return res.status(200).json({
+            nome : usuarioLogado.nome,email: usuarioLogado.email,token
+        });
+    }
+
+    return res.status(400).json({ erro: 'Usuário ou senha inválidos' });
 };
 
 export default conectarMongoDB(endpointLogin);
