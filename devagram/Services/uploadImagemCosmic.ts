89d3298e9a5d2ea934840b597ab6132305744176
Victor Augusto da Silva <victor1231967@hotmail.com>
2024-05-24 17:23:09 -0300

    Conexão entre API Cosmic com o endpoint

---
+++ devagram/Services/uploadImagemCosmic.ts
@@ -0,0 +1,47 @@
+import { createBucketClient} from "@cosmicjs/sdk";
+import multer from "multer";
+
+
+const{BUCKET_SLUG,READ_KEY,WRITE_KEY}= process.env;
+
+const bucketDevagram = createBucketClient({
+    bucketSlug: BUCKET_SLUG as string,
+    readKey: READ_KEY as string,
+    writeKey: WRITE_KEY as string
+});
+const storage = multer.memoryStorage();
+const upload = multer({ storage : storage});
+
+const uploadImagemCosmic = async(req: any) =>{
+    if(req?.file?.originalname){
+        if(
+            !req.file.originalname.includes(".png") &&
+            !req.file.originalname.includes(".jpg") &&
+            !req.file.originalname.includes(".jpeg") 
+        ){
+            throw new Error("Extensão da imagem inválida");
+        }
+        const media_object = {
+            originalname: req.file.originalname,
+            buffer: req.file.buffer,
+        };
+        if(req.url && req.url.includes("publicacao")){
+            return await bucketDevagram.media.insertOne({
+                media: media_object,
+                folder: "publicacao",
+            });
+        }else if (req.url && req.url.includes("usuario")){
+            return await bucketDevagram.media.insertOne({
+                media: media_object,
+                folder: "avatar",
+            });
+        }else {
+            return await bucketDevagram.media.insertOne({
+                media: media_object,
+                folder: "stories",
+            });
+        }
+        }
+    };
+
+    export {upload,uploadImagemCosmic};
