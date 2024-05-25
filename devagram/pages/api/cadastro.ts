a86a877356a836ff2e84c0e31f16753ab66cdc15
Victor Augusto da Silva <victor1231967@hotmail.com>
2024-05-24 21:38:24 -0300

    Conexão com cosmic
     Alteração da tela de cadastro para envio de avatar, conexão com cosmic

--- devagram/pages/api/cadastro.ts
+++ devagram/pages/api/cadastro.ts
@@ -1,40 +1,83 @@
-// cadastro.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { conectarMongoDB} from '../../middlewares/conectarMongoDB';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
 import type { CadastroRequisicao } from '../../types/CadastroRequisicao';
 import { UsuarioModel } from '../../Models/UsuarioModel';
 import md5 from 'md5';
+import multer from 'multer';
+import { createBucketClient } from "@cosmicjs/sdk";
+import nc from 'next-connect';
+import { upload,uploadImagemCosmic } from '../../Services/uploadImagemCosmic';
 
-const endpointCadastro = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
-    if (req.method === 'POST') {
-        const usuario = req.body as CadastroRequisicao;
+const handler = nc() // Utilizando o nc do Next Connect que serve para "gerenciar" as requisições e resposta HTTP
+  .use(upload.single("file")) // utiliza o Multer para fazer upload de um único arquivo
+  .post(
+    // Utiliza o método POST do NC
+    async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+      try {
+        // Trativa de erro, caso dê algum erro na tentativa o catch pegará esse erro e será possível tratar ele
+        const usuario = req.body as CadastroRequisicao; // Criação do corpo da Request conforme o tipo padrão que criamos
+
+        // Verificações de Nome, Email e Senha. Não é necessário a de imagem pois não é obrigatório
+
         if (!usuario.nome || usuario.nome.length < 2) {
-            return res.status(400).json({ erro: 'Nome inválido' });
+          // Verifica se há nome e se ele tem um tamanho maior que 2, caso não atenda retorna erro de Nome inválido
+          return res.status(400).json({ erro: "Nome inválido" });
         }
-        if (!usuario.email || usuario.email.length < 5 || !usuario.email.includes('@') || !usuario.email.includes('.')) {
-            return res.status(400).json({ erro: 'E-mail inválido' });
+
+        if (
+          // Verifica se o e-mail tem uma estrutura simples de email:
+          // -Verifica se existe
+          // -Verifica se tem mais que 5 caracteres
+          // -Verifica se possui o @
+          // -Verifica se tem um .
+          !usuario.email ||
+          usuario.email.length < 5 ||
+          !usuario.email.includes("@") ||
+          !usuario.email.includes(".")
+        ) {
+          return res.status(400).json({ erro: "Email inválido" }); // Retorna Email inválido caso qualquer uma das condições não sejam atendidas
         }
         if (!usuario.senha || usuario.senha.length < 4) {
-            return res.status(400).json({ erro: 'Senha inválida' });
-        }
-        //Validação se ja existe usuario com o mesmo email
-        const usuariosComMesmoEmail = await UsuarioModel.find({email: usuario.email});
-        if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0 ){
-            return res.status(400).json({erro: 'Ja existe uma conta com o e-mail informado'})
+          // Verifica o tamanho da senha caso não seja maior que 4 retorna um erro
+          return res.status(400).json({ erro: "Senha inválida" });
         }
 
-        // Salva no banco de dados
-        const ususarioSalvo = {
-            nome: usuario.nome,
-            email: usuario.email,
-            senha: md5(usuario.senha),
+        // Verificando se já existe usuário com o mesmo email que foi informado no cadastro
+
+        const usuarioComMesmoEmail = await UsuarioModel.find({
+          // Método de pesquisa em um Model, é passado os parâmetros e é retornado um objeto que contenha o que foi passado
+          email: usuario.email, // Passando um JSON com o usuário do cadastro
+        });
+        if (usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0) {
+          // Caso a pesquisa retorne algo e esse retorno tenha um tamanho maior que 0 retorna um erro de que já existe um cadastro com esse email
+          return res
+            .status(400)
+            .json({ erro: "Esse email já está cadastrado" });
         }
-        await UsuarioModel.create(ususarioSalvo);
-        return res.status(200).json({ msg: 'Usuário criado com sucesso' });
+        // enviar a imagem do multer para o cosmic
+        const image = await uploadImagemCosmic(req); 
+
+        // salvar no banco de dados
+        const usuarioASerSalvo = { // Cria uma const com o que é esperado de um usuario e isso foi definido no Model
+          nome: usuario.nome,
+          email: usuario.email,
+          senha: md5(usuario.senha), // Utilização do md5 para que o dado seja guardado criptografado, caso haja algum vazamento a senha ao menos estará com uma criptografia
+          avatar: image?.media?.url, // Caso exista uma imagem ela será mandada para o avatar do usuário
+        };
+
+        await UsuarioModel.create(usuarioASerSalvo); // O método create cria o objeto definido em usuarioASerSalvo na Coleção
+        return res.status(200).json({ msg: "Usuário cadastrado com sucesso" });
+      } catch (e) {
+        console.log(e);
+        return res.status(500).json({ erro: "erro ao cadastrar o usuário" });
+      }
     }
-    return res.status(405).json({ erro: 'Método informado não é válido' });
+  );
+export const config = { // Configurando para que o NEXT não mande uma request com um JSON, por estarmos enviando uma foto é necessário que seja um FormData
+  api: {
+    bodyParser: false,
+  },
 };
-//primeiro conecta depois roda o endpoint
-export default conectarMongoDB(endpointCadastro);
- 
+export default conectarMongoDB(handler);
+export {upload,uploadImagemCosmic};
