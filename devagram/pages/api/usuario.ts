1fe6efe51eaaf69b5f2848882c7329d9cb5a7447
Victor Augusto da Silva <victor1231967@hotmail.com>
2024-06-17 17:03:13 -0300

    Adição de um novo endpoint do usuario.ts

--- devagram/pages/api/usuario.ts
+++ devagram/pages/api/usuario.ts
@@ -3,6 +3,31 @@ import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
 import  {conectarMongoDB} from '../../middlewares/conectarMongoDB'
 import {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg'
 import { UsuarioModel } from '../../Models/UsuarioModel';
+import nc from 'next-connect';
+import {upload,uploadImagemCosmic} from '../../Services/uploadImagemCosmic'
+
+const handler = nc () 
+.use(upload.single('avatar')) 
+.put(async(req : NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>)=>{
+    try{
+        const{userId} = req?.query;
+        const usuario = await UsuarioModel.findById(userId);
+
+        //se o usuario foi encontrado
+        if(!usuario){
+            return res.status(400).json({erro: 'Usuario não encontrado'});
+        }   
+        const {nome} =  req.body;
+        if (nome && nome.length >2 ){
+            return res.status(400).json({erro: 'Nome invalido'});  
+        }
+    }
+    catch(e){
+        console.log(e);
+    }
+   return res.status(400).json({erro: 'Não foi possivel atualizar o usuario'});
+}
+);
 
 const usuarioEndpoint =  async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>)=>{
     
