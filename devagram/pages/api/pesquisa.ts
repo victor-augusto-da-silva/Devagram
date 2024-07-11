acfa38aa79a04f79419af3570d3ed307116a7b0b
Victor Augusto da Silva <victor1231967@hotmail.com>
2024-07-11 15:02:46 -0300

    Atualização da aba de pesquisa

--- devagram/pages/api/pesquisa.ts
+++ devagram/pages/api/pesquisa.ts
@@ -1,57 +1,51 @@
-import type {NextApiRequest,NextApiResponse} from 'next';
-import type {RespostaPadraoMsg	} from '../../types/RespostaPadraoMsg';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
 import { UsuarioModel } from '../../Models/UsuarioModel';
 
-const pesquisaEndPoint = async(req: NextApiRequest, 
-    res:  NextApiResponse<RespostaPadraoMsg | any[]>)=>{
-    try{
-        if(req.method === 'GET'){
-
-            if(req?.query?.id){
-
-                const usuariosEncontrado = await UsuarioModel.findById(req?.query?.id)
-                if(!usuariosEncontrado){
-                    return res.status(400).json({erro: 'Usuario não encontrado'});
-     
-                }
-                    usuariosEncontrado.senha = null;
-                    return res.status(200).json(usuariosEncontrado);
-                
-            }
-            else {
-                const {filtro} = req.query;
-                // busca pelo nome,e-mail...etc
-                //ira percorrer todos os campos
-                if(!filtro || filtro.length <2 )
-                    {
-                        return res.status(400).json({erro: 'Favor informar pelo menos 2 caracteres para busca'});
-    
-                    }
-                    const usuariosEncontrados = await UsuarioModel.find({
-                       // faz com que não fique case sensitive
-                       // o i é de ignore case (busca indiferente do case sensitive)
-                        $or: [{nome: {$regex: filtro , $options : 'i'}},
-                            {email: {$regex: filtro , $options : 'i'}}]
-                    });
-                return res.status(200).json(usuariosEncontrados);
-            }
-
-
-            }
-
-
-         
-        return res.status(405).json({erro : 'Metodo informado não é valido' })
-    }   
-
-    catch(e){
-        console.log(e);
-        return res.status(500).json({erro : 'Não foi possível buscar usuarios: ' + e})
+const pesquisaEndPoint = async (
+  req: NextApiRequest,
+  res: NextApiResponse<RespostaPadraoMsg | any[]>
+) => {
+  try {
+    if (req.method === 'GET') {
+      if (req?.query?.id) {
+        const usuariosEncontrado = await UsuarioModel.findById(req?.query?.id);
+        if (!usuariosEncontrado) {
+          return res.status(400).json({ erro: 'Usuário não encontrado' });
+        }
+        // Removendo a senha antes de retornar o usuário
+        usuariosEncontrado.senha = undefined;
+        return res.status(200).json(usuariosEncontrado);
+      } else {
+        const { filtro } = req.query;
+        if (!filtro || filtro.length < 2) {
+          return res
+            .status(400)
+            .json({ erro: 'Favor informar pelo menos 2 caracteres para busca' });
+        }
+        const usuariosEncontrados = await UsuarioModel.find({
+          $or: [
+            { nome: { $regex: filtro, $options: 'i' } },
+            { email: { $regex: filtro, $options: 'i' } },
+          ],
+        });
+        // Removendo a senha de todos os usuários encontrados
+        const usuariosSemSenha = usuariosEncontrados.map((usuario) => {
+          usuario.senha = undefined;
+          return usuario;
+        });
+        return res.status(200).json(usuariosSemSenha);
+      }
     }
+    return res.status(405).json({ erro: 'Método informado não é válido' });
+  } catch (e) {
+    console.log(e);
+    return res
+      .status(500)
+      .json({ erro: 'Não foi possível buscar usuários: ' + e });
+  }
+};
 
-}
-
-
-export default validarTokenJWT( conectarMongoDB(pesquisaEndPoint));
+export default validarTokenJWT(conectarMongoDB(pesquisaEndPoint));
