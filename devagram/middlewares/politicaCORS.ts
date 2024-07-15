941cae1ec5f49829201b3862aedf66485803dcfd
Victor Augusto da Silva <victor1231967@hotmail.com>
2024-07-14 21:40:39 -0300

    Criação do Middller de CORS

---
+++ devagram/middlewares/politicaCORS.ts
@@ -0,0 +1,27 @@
+import {NextApiRequest,NextApiResponse,NextApiHandler} from 'next'
+import {RespostaPadraoMsg}  from '../types/RespostaPadraoMsg';
+import NextCors from 'nextjs-cors';
+import { METHODS } from 'http';
+import { Postpone } from 'next/dist/server/app-render/dynamic-rendering';
+import { pathToFileURL } from 'url';
+
+
+export const politicaCORS = (hander : NextApiHandler ) => 
+    async(req : NextApiRequest , res: NextApiResponse<RespostaPadraoMsg>) =>{
+        try{
+
+            await NextCors(req,res,{
+                    origin : '*',
+                    methods : ['GET','POST','PUT'],
+                    //Navegadores antigos dão problema com o 204 (No Content)
+                    optionSuccessStatus : 200,
+                    
+            });
+            
+            return hander(req,res);
+        }
+        catch(e){
+            console.log('Erro ao tratar a exceção: ' + e );
+            res.status(500).json({erro: 'Ocorreu erro ao tratar politica de CORS'})
+        }
+}
